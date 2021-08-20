4fc8e98a9b70396d58b670b3fb66036f32f497e7
kermage <genealyson.torcende@gmail.com>
2021-08-20 16:29:56 +0800

    clearer resource response

--- app/Http/Resources/Wallet.php
+++ app/Http/Resources/Wallet.php
@@ -14,6 +14,9 @@ class Wallet extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'address' => $this->address,
+            'flagged' => true,
+        ];
     }
 }
--- app/Http/Resources/WalletCollection.php
+++ app/Http/Resources/WalletCollection.php
@@ -14,6 +14,6 @@ class WalletCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->collection->pluck('address')->toArray();
     }
 }
