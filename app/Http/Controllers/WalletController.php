42d48eada2896466180ed3073dbfe0a1a3440043
kermage <genealyson.torcende@gmail.com>
2021-08-20 14:38:17 +0800

    make wallet

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wallet;
+use Illuminate\Http\Request;
+
+class WalletController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Wallet  $wallet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Wallet $wallet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Wallet  $wallet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Wallet $wallet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Wallet  $wallet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Wallet $wallet)
+    {
+        //
+    }
+}
---
+++ database/factories/WalletFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Wallet;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WalletFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Wallet::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
